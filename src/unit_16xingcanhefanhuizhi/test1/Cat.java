32cd9da061d684587356496038d3c583f7b2ae4e
grape777 <1715161816@qq.com>
2022-07-28 10:39:35 +0800

    类名作为形参和返回值

---
+++ src/unit_16xingcanhefanhuizhi/test1/Cat.java
@@ -0,0 +1,8 @@
+package unit_16xingcanhefanhuizhi.test1;
+
+public class Cat {
+
+    public void eat(){
+        System.out.println("猫吃鱼");
+    }
+}
---
+++ src/unit_16xingcanhefanhuizhi/test1/CatOperator.java
@@ -0,0 +1,13 @@
+package unit_16xingcanhefanhuizhi.test1;
+
+public class CatOperator {
+
+    public void useCat(Cat c){ //Cat c = new Cat();
+        c.eat();
+    }
+
+    public Cat getCat(){
+        Cat c = new Cat();
+        return c;
+    }
+}
