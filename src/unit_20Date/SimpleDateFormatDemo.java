cbd93f8ef6854834c1adb16bdd5b8feac6f4b111
grape777 <1715161816@qq.com>
2022-08-01 16:15:33 +0800

    SimpleDateFormat测试

---
+++ src/unit_20Date/SimpleDateFormatDemo.java
@@ -0,0 +1,25 @@
+package unit_20Date;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SimpleDateFormatDemo {
+    public static void main(String[] args) throws ParseException {
+        //格式化：从Date到String
+        Date d = new Date();
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss");
+        String s = sdf.format(d);
+        System.out.println(s);
+        System.out.println("---------------");
+
+
+        //从String到Date
+        String ss = "2022-08-01 16:12:00";
+        //ParseException
+        SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date dd = sdf2.parse(ss);
+        System.out.println(dd);
+    }
+}
